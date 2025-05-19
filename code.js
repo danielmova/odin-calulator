5f1359ba0759027262f148a80ca09a74aa42066c
danielmova <dataxf@gmail.com>
2025-05-19 12:56:11 -0500

    Rewriting the code to undestad better

--- code.js
+++ code.js
@@ -1,69 +1,80 @@
-let firstNumber = "";
-
 let operator = "";
+let valuePrev = "";
+let valueNow = "";
 
-let secondNumber = "";
+document.addEventListener("DOMContentLoaded", function(){
+    let displayNow = document.querySelector(".now");
+    let displayPrev = document.querySelector(".prev");
 
-let tempNumber = "";
+    let clear = document.querySelector(".clear");
+    let erase = document.querySelector(".erase");
+    let dot = document.querySelector(".dot");
+    let equal = document.querySelector(".equal");
 
-const display = document.querySelector(".display");
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
 
-const buttons = document.querySelectorAll("button");
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent);
+        displayNow.textContent = valueNow;
+    }))
 
-function addOperation(a, b){
-    return parseInt(a) + parseInt(b);
-}
+    operators.forEach((op) => op.addEventListener("click", function(e){
+        handleOperator(e.target.textContent);
+        displayPrev.textContent = valuePrev + " " + operator;
+        displayNow.textContent = valueNow;
+    }))
 
-function subtractOperation(a, b){
-    return parseInt(a) - parseInt(b);
-}
+    clear.addEventListener("click", function(){
+        valueNow = "";
+        valuePrev = "";
+        operator = "";
+        displayNow.textContent = "";
+        displayPrev.textContent = "";
+    })
 
-function multiplyOperation(a, b){
-    return parseInt(a) * parseInt(b);
-}
-
-function divideOperation(a, b){
-    return parseInt(a) / parseInt(b);
-}
-
-function operate(){
-    firstNumber = prompt("First number", 0);
-    operator = prompt("Operator", "+");
-    secondNumber = prompt("Second number", 0);
-
-    if (operator === "+"){
-        return addOperation(firstNumber, secondNumber)
-    }
-    else if (operator === "-"){
-        return subtractOperation(firstNumber, secondNumber)
-    } 
-    else if (operator === "*"){
-        return multiplyOperation(firstNumber, secondNumber)
-    } 
-    else {
-        return divideOperation(firstNumber, secondNumber)
-    } 
-}
-
-function updateDisplay(){
-    display.innerText = tempNumber;
-}
-
-buttons.forEach(button => button.addEventListener("click", evaluateInput));
-
-function evaluateInput(event) {
-    const button = event.target;
-    
-    if(button.classList.contains("number")) {
-        tempNumber += button.innerText;
-        updateDisplay();
-    } else {
-        if(button.classList.contains("operator") && firstNumber === "" && operator === ""){
-            firstNumber = tempNumber;
-            operator = button.innerText;
-            console.log(`${firstNumber}...${operator}`);
-            tempNumber = button.innerText;
-            updateDisplay();
+    equal.addEventListener("click", function(){
+        calculate();
+        displayPrev.textContent = "";
+        if(valuePrev.length <= 5){
+            displayNow.textContent = valuePrev;    
+        } else{
+            displayNow.textContent = valuePrev.slice(0,5) + "...";
         }
+    })
+});
+
+function handleNumber(num){
+    if(valueNow.length <= 5){
+        valueNow += num;
     }
+};
+
+function handleOperator(op){
+    operator = op;
+    valuePrev = valueNow;
+    valueNow = "";
+}
+
+function calculate(){
+    valueNow = Number(valueNow);
+    valuePrev = Number(valuePrev);
+
+    if(operator === "+"){
+        valuePrev += valueNow;
+    } else if (operator === "-"){
+        valuePrev -= valueNow;
+    } else if (operator === "X"){
+        valuePrev *= valueNow;
+    } else if (operaror === "/"){
+        valuePrev /= valueNow;
+    }
+
+    valuePrev = roundNumber(valuePrev);
+    valuePrev = valuePrev.toString();
+    valueNow = valuePrev.toString();
+}
+
+function roundNumber(num){
+    return Math.round(num * 1000) / 1000;
 }
